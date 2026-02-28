95b4882865cc4234a6935ae4c40011829fb5150d
Muhammed Suhail M <muhammedsuhail@Muhammeds-MacBook-Air.local>
2026-02-28 10:38:12 +0530

    duplication removal

--- pro-project/pro-project/github-oidc/oidc_provider.tf
+++
@@ -1,4 +0,0 @@
-module "github_oidc" {
-  source  = "../modules/terraform-aws-iam-6.4.0/modules/iam-oidc-provider"
-
-}
--- pro-project/pro-project/github-oidc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "project_name" {
-  description = "AWS region"
-  type        = string
-}
