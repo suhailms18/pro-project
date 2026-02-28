600ed5e3c3e49704fce7a7eefd5c63ba8779ffaa
Muhammed Suhail M <muhammedsuhail@Muhammeds-MacBook-Air.local>
2026-02-28 10:07:32 +0530

    pushing modules folder

---
+++ pro-project/pro-project/github-oidc/oidc_role.tf
@@ -0,0 +1,24 @@
+module "github_oidc_iam_role" {
+  source = "../modules/terraform-aws-iam-6.4.0/modules/iam-role"
+
+  name = "${var.project_name}-oidc-role"
+  use_name_prefix = false
+
+  enable_github_oidc = true
+
+  oidc_subjects = [
+    "repo:suhailms18/pro-project:pull_request",
+    "suhailms18/pro-project:ref:refs/heads/master",
+  ]
+
+  policies = {
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-oidc-role"
+    ManagedBy   = "Terraform"
+    Project     = "pro-project"
+    Purpose     = "GitHub OIDC Role"
+  }
+}
