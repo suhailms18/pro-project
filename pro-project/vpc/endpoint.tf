e7509a5a9085aa43ed4dda8bff40afe6db7a1c8c
Muhammed Suhail M <muhammedsuhail@Muhammeds-MacBook-Air.local>
2026-03-07 15:59:41 +0530

    creating dedicated security groups for vpc endpoints

--- pro-project/vpc/endpoint.tf
+++ pro-project/vpc/endpoint.tf
@@ -4,12 +4,15 @@ module "vpc_endpoints" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  security_group_ids = [module.vpce_sg[each.key].security_group_id]
-
   endpoints = {
     for k, v in var.vpc_endpoints :
     k => merge(
       v,
+
+      try(v.service_type, "Interface") != "Gateway" ? {
+        security_group_ids = [module.vpce_sg[k].security_group_id]
+      } : {},
+
       try(v.service_type, "Interface") == "Gateway" ? {
         route_table_ids = concat(
           module.vpc.private_route_table_ids,
